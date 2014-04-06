78336996d322e9c8e704c85b8fda1be9e10cf6d8
Kim MinJung <Reny@Kimui-MacBook-Air.local>
2014-04-06 22:45:11 +0900

    Add tileeditor script.

---
+++ Assets/Editor/TileEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Tile))]
+public class TileEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+        Tile tile = target as Tile;
+
+		base.OnInspectorGUI ();
+
+		if (GUI.changed)
+        {
+            Debug.LogWarning ("Tile gui changed.");
+            tile.gameObject.SetActive(tile.isVisible);
+            EditorUtility.SetDirty (target);
+        }
+	}
+}
