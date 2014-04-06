b35caa10b411db08482fd4cfa931aaaae668d79b
Kim MinJung <Reny@Kimui-MacBook-Air.local>
2014-04-06 22:28:46 +0900

    isVisible func. added.

--- Assets/TestField/TestFieldTiles.cs
+++ Assets/TestField/TestFieldTiles.cs
@@ -5,13 +5,20 @@ public class TestFieldTiles : MonoBehaviour {
 
 	public Tile tile;
 
+	const int MapWidth = 15;
+	const int MapHeight = 15;
+
 	public void GenerateTile ()
 	{
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 13; j++) {
+		for (int i = 0; i < MapWidth; i++) {
+			for (int j = 0; j < MapHeight; j++) {
 				Vector2 tilePosition;
 				tilePosition = TestFieldManager.getTranslatedPosition (i, j);
-				Tile tileInstance = Instantiate (tile, new Vector3 (tilePosition.x, tilePosition.y, j*0.1f), Quaternion.identity) as Tile;
+
+//				float halfWidth = TestFieldManager.unityTileX * (MapWidth-1) / 2;
+//				float halfHeight = (TestFieldManager.unityTileY * (MapHeight-1) - (0.15f * (MapHeight-2))) / 2;
+				Vector3 zerozeroPosition = new Vector3 (tilePosition.x, tilePosition.y, j*0.1f);
+				Tile tileInstance = Instantiate (tile, zerozeroPosition, Quaternion.identity) as Tile;
 
 				GameObject tileInstanceGameObject = tileInstance.gameObject;
 				GameObject testFieldTilesGameObject = gameObject;
@@ -29,5 +36,4 @@ public class TestFieldTiles : MonoBehaviour {
     {
         GenerateTile ();
     }
-
 }
--- Assets/TestField/Tile.cs
+++ Assets/TestField/Tile.cs
@@ -3,9 +3,14 @@ using System.Collections;
 
 public class Tile : MonoBehaviour {
 
+	public bool isVisible;
+
 	// Use this for initialization
 	void Start () {
-
+		if (isVisible == false) 
+		{
+			gameObject.SetActive(false);
+		}
 	}
 	
 	// Update is called once per frame
