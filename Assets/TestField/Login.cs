3c8978cf4834677e1b6c23449dcc1b4158cff05a
MSapiens <kiki9014@gmail.com>
2014-04-27 18:07:58 +0900

    add NetworkManager

---
+++ Assets/TestField/Login.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour {
+	string username="";
+	float height_unit = Screen.height / 720f * 1.4f;
+	float width_unit =  Screen.width / 1280f * 1.4f;
+	public GUISkin guiskin;
+
+	// Use this for initialization
+	void Start () {
+		height_unit = Screen.height / 720f;
+		guiskin.textField.fontSize = (int) (20 * height_unit);
+		guiskin.label.fontSize = (int) (20 * height_unit);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.skin = guiskin;
+		username = GUI.TextField(new Rect(10*width_unit,10*height_unit,100*width_unit,40*height_unit),username);
+		PlayerPrefs.SetString ("id", username);
+		if(GUI.Button(new Rect(10*width_unit,60*height_unit,100*width_unit,40*height_unit),"Login")){
+			//username = "";
+			Application.LoadLevel("Lobby");
+		}
+	}
+}
