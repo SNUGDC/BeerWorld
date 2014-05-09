00cc7d844ae45d699a9c2308f940d82acc179e47
gon <gon@WORKGROUP.local>
2014-05-09 14:25:34 +0900

    애니메이션 주사위 굴리기 씬 추

---
+++ Assets/Resources/Dice/DiceAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiceAnimation : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void rollByNumber(int num){
+		anim.SetInteger("num",num);
+		anim.SetTrigger("roll");
+	}
+}
---
+++ Assets/Resources/Dice/TestDice.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestDice : MonoBehaviour {
+	DiceAnimation diceCon;
+	int diceNum = 5;
+	// Use this for initialization
+	void Start () {
+		diceCon = GetComponent<DiceAnimation>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log("Click");
+			diceCon.rollByNumber(diceNum);
+			switch(diceNum){
+			case 5:
+				diceNum=2;
+				break;
+			case 2:
+				diceNum=4;
+				break;
+			case 4:
+				diceNum=5;
+				break;
+			}
+		}
+	}
+
+}
