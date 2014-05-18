db1634b3a37830cffa28ca69a80719105d1c1070
momamene <momamene@gmail.com>
2014-05-18 12:09:01 +0900

    RoomManager=>LobbyGUIView, ChatGUIView, RoomGUIView

---
+++ Assets/Scripts/Const.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Const {
+    public static readonly float GUI_HEIGHT_UNIT = Screen.height / 720f * 1.4f;
+    public static readonly float GUI_WIDTH_UNIT = Screen.width / 1280f * 1.4f;
+}
---
+++ Assets/Scripts/GUI/RoomGUIView.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomGUIView : MonoBehaviour {
+    public GUISkin guiSkin;
+    
+    // Use this for initialization
+	void Start () {
+        guiSkin.textField.fontSize = (int)(20 * Const.GUI_HEIGHT_UNIT);
+        guiSkin.label.fontSize = (int)(20 * Const.GUI_HEIGHT_UNIT);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = guiSkin;
+        if(Network.peerType == NetworkPeerType.Server){
+		    GUI.Label(new Rect(10*Const.GUI_WIDTH_UNIT,10*Const.GUI_HEIGHT_UNIT,200*Const.GUI_WIDTH_UNIT,40*Const.GUI_HEIGHT_UNIT),"Server");
+			GUI.Label(new Rect(10*Const.GUI_WIDTH_UNIT,60*Const.GUI_HEIGHT_UNIT,200*Const.GUI_WIDTH_UNIT,40*Const.GUI_HEIGHT_UNIT),"# of Member : "+(Network.connections.Length+1));
+
+			if(GUI.Button(new Rect(210*Const.GUI_WIDTH_UNIT,10*Const.GUI_HEIGHT_UNIT,200*Const.GUI_WIDTH_UNIT,40*Const.GUI_HEIGHT_UNIT), "Out")){
+				Network.Disconnect(250);
+                FindObjectOfType<ChatGUIView>().ClearTextToRead();
+				Application.LoadLevel("Lobby");
+				Destroy(this.gameObject);
+			}
+		}
+		if(Network.peerType == NetworkPeerType.Client){
+			GUI.Label(new Rect(10*Const.GUI_WIDTH_UNIT,10*Const.GUI_HEIGHT_UNIT,200*Const.GUI_WIDTH_UNIT,40*Const.GUI_HEIGHT_UNIT),"Client");
+			GUI.Label(new Rect(10*Const.GUI_WIDTH_UNIT,60*Const.GUI_HEIGHT_UNIT,200*Const.GUI_WIDTH_UNIT,40*Const.GUI_HEIGHT_UNIT),"# of Member : "+(Network.connections.Length+1));
+
+			if(GUI.Button(new Rect(210*Const.GUI_WIDTH_UNIT,10*Const.GUI_HEIGHT_UNIT,100*Const.GUI_WIDTH_UNIT,60*Const.GUI_HEIGHT_UNIT),"Out")){
+				Network.Disconnect(250);
+                FindObjectOfType<ChatGUIView>().ClearTextToRead();
+				Application.LoadLevel("Lobby");
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
