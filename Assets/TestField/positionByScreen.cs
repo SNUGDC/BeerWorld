2bd62fdc324c30858b418054419838134f853e48
gon <gon@172.20.nate.com>
2014-05-18 16:17:48 +0900

    Visual work for monday :D modification in TestField_art.unity

---
+++ Assets/TestField/positionByScreen.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class positionByScreen : MonoBehaviour {
+	public bool positionInRight = false;
+	public float distanceFromEdge = 1f;
+
+	private float width;
+	private float pixelPerUnit;
+	private float widthByPixel;
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = transform.parent.camera;
+		widthByPixel = Screen.width;
+		pixelPerUnit = Screen.height/(cam.orthographicSize*2);
+		width = widthByPixel/pixelPerUnit;
+
+		if(positionInRight){
+			Vector3 buttonPosition = transform.localPosition;
+			buttonPosition.x = width/2 - distanceFromEdge;
+			transform.localPosition = buttonPosition;
+		}else{
+			Vector3 buttonPosition = transform.localPosition;
+			buttonPosition.x = distanceFromEdge - width/2;
+			transform.localPosition = buttonPosition;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
