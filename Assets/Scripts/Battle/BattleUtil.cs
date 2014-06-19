530b2fc610091f8cb1975daaf7839ef634db4243
Juhyeong Park <majecty@gmail.com>
2014-06-19 22:19:04 +0900

    Add structure of battle scene.

---
+++ Assets/Scripts/Battle/BattleUtil.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class BattleUtil
+{
+    public static BattlePlayer GetDummyPlayer()
+    {
+        List<BDice.Species> attackDices = new List<BDice.Species>();
+        List<BDice.Species> defenseDices = new List<BDice.Species>();
+
+        attackDices.Add(BDice.Species.Six);
+        attackDices.Add(BDice.Species.Six);
+        attackDices.Add(BDice.Species.Six);
+
+        defenseDices.Add(BDice.Species.Six);
+
+        return new BattlePlayer(attackDices, defenseDices,
+                10);
+    }
+
+    public static BattlePlayer GetDummyEnemy()
+    {
+        List<BDice.Species> attackDices = new List<BDice.Species>();
+        List<BDice.Species> defenseDices = new List<BDice.Species>();
+
+        attackDices.Add(BDice.Species.Six);
+        attackDices.Add(BDice.Species.Six);
+
+        defenseDices.Add(BDice.Species.Six);
+        defenseDices.Add(BDice.Species.Six);
+
+        return new BattlePlayer(attackDices, defenseDices,
+                5);
+    }
+}
