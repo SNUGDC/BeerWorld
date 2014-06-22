977c409caaf7e88748e138f2a5b370a658b59285
Juhyeong Park <majecty@gmail.com>
2014-06-22 20:55:31 +0900

    Refactor character move logic.

---
+++ Assets/Scripts/Character/CharacterMover.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour
+{
+    public void MoveTo(int tileCoordX, int tileCoordY)
+    {
+        Tile tile = TileManager.GetTileByCoord(tileCoordX, tileCoordY);
+        MoveTo(tile);
+    }
+
+    public void MoveTo(Tile toMoveTile)
+    {
+        Vector2 nextTilePosition = new Vector2(
+            toMoveTile.transform.position.x, 
+            toMoveTile.transform.position.y);
+        Vector2 nextTileCoordinate =
+            FieldTileUtility.GetCoordFromPosition(
+                nextTilePosition.x, 
+                nextTilePosition.y);
+
+        transform.position = new Vector3(nextTilePosition.x, nextTilePosition.y, Character.Depth);
+        Vector2 newCoordinate = FieldTileUtility.GetCoordFromPosition(nextTilePosition.x, nextTilePosition.y);
+    }
+}
