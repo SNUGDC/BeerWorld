0646157aa4a4ad756b009e7260080d7cd21aad43
Minjung Kim <Reny.atkins@gmail.com>
2014-07-27 15:36:37 +0900

    conflict fix

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -5,12 +5,6 @@ public class Character : MonoBehaviour {
 
 	public static readonly int Depth = -3;
 
-<<<<<<< HEAD
-	public int currentTileKey;
-	public int preTileKey;
-	public int prePreTileKey;
-
-=======
 	public int currentTileKey
 	{
 		get;
@@ -28,8 +22,7 @@ public class Character : MonoBehaviour {
 		get;
 		private set;
 	}
-	
->>>>>>> 8e2d5a9fd3b934fcc58f9cab0aad2d851e2e1966
+
 	public int maxHp;
 	public int currentHp;
 
@@ -42,7 +35,7 @@ public class Character : MonoBehaviour {
 	public int numberOfMoveDice;
 	public BDice.Species speciesOfMoveDice;
 
-	public List<Item> itemList = new List<Item>();
+	//public List<Item> itemList = new List<Item>();
 
 
 	// Use this for initialization = BDice.Species.Six;
@@ -62,12 +55,6 @@ public class Character : MonoBehaviour {
 		speciesOfMoveDice = BDice.Species.Six;
 	}
 
-<<<<<<< HEAD
-	// Update is called once per frameb
-	void Update ()
-	{
-
-=======
 	public void InitializeTileKey(int tileKey)
 	{
 		currentTileKey = tileKey;
@@ -81,6 +68,5 @@ public class Character : MonoBehaviour {
 		prePreTileKey = preTileKey;
 		preTileKey = currentTileKey;
 		currentTileKey = tileKey;
->>>>>>> 8e2d5a9fd3b934fcc58f9cab0aad2d851e2e1966
 	}
 }
