b1082aac23e475e221e7ab5a16392009c05866da
Juhyeong Park <majecty@gmail.com>
2014-08-02 16:15:40 +0900

    Add local test shortcut.

---
+++ Assets/Scripts/DevTest/DevTestStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevTestStart : MonoBehaviour
+{
+	void Start()
+	{
+		Network.InitializeServer(3, 25008, false);
+	}
+
+	void OnServerInitialized()
+	{
+		Application.LoadLevel("Battle");
+	}
+}
---
+++ Assets/Scripts/DevTest/Editor/DevStartMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class DevStartMenu : EditorWindow
+{
+	[MenuItem("Test/DevRun %#p")]
+	static void Run()
+	{
+		EditorApplication.OpenScene("Assets/Scenes/DevTest.unity");
+		EditorApplication.isPlaying = true;
+	}
+}
