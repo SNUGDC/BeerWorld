4ef9740e76f616db5c00cb1bfe91a1eb195e8675
Juhyeong Park <majecty@gmail.com>
2014-08-03 16:17:10 +0900

    Reformat chracter mover.cs

--- Assets/Scripts/Unit/CharacterMover.cs
+++ Assets/Scripts/Unit/CharacterMover.cs
@@ -35,26 +35,26 @@ public class CharacterMover : MonoBehaviour
 		currentTileKey = tileKey;
 	}
 
-    public void MoveTo(int tileKey)
-    {
-        Tile tile = TileManager.GetExistTile(tileKey);
-        MoveTo(tile);
-    }
+	public void MoveTo(int tileKey)
+	{
+		Tile tile = TileManager.GetExistTile(tileKey);
+		MoveTo(tile);
+	}
 
-    public void MoveTo(int tileCoordX, int tileCoordY)
-    {
-        Tile tile = TileManager.GetTileByCoord(tileCoordX, tileCoordY);
-        MoveTo(tile);
-    }
+	public void MoveTo(int tileCoordX, int tileCoordY)
+	{
+		Tile tile = TileManager.GetTileByCoord(tileCoordX, tileCoordY);
+		MoveTo(tile);
+	}
 
-    public void MoveTo(Tile toMoveTile)
-    {
-        Vector2 nextTilePosition = new Vector2(
-            toMoveTile.transform.position.x, 
-            toMoveTile.transform.position.y);
+	public void MoveTo(Tile toMoveTile)
+	{
+		Vector2 nextTilePosition = new Vector2(
+				toMoveTile.transform.position.x,
+				toMoveTile.transform.position.y);
 
-        transform.position = new Vector3(nextTilePosition.x, nextTilePosition.y, Character.Depth);
-				int currentTileKey = FieldTileUtility.GetKeyFromTile(toMoveTile);
-				UpdateTileKey(currentTileKey);
-    }
+		transform.position = new Vector3(nextTilePosition.x, nextTilePosition.y, Character.Depth);
+		int currentTileKey = FieldTileUtility.GetKeyFromTile(toMoveTile);
+		UpdateTileKey(currentTileKey);
+	}
 }
