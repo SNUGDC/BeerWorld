98d2a74cd423b32b611c0e740a807b7d47dead7f
Juhyeong Park <majecty@gmail.com>
2014-08-10 16:24:48 +0900

    Reformat enemy place holder.

--- Assets/Scripts/Unit/EnemyPlaceHolder.cs
+++ Assets/Scripts/Unit/EnemyPlaceHolder.cs
@@ -1,17 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections.Generic;
 
 public class EnemyPlaceHolder
 {
-    private List<int> enemyPlaces = new List<int>();
+  private List<int> enemyPlaces = new List<int>();
 
-    public EnemyPlaceHolder()
-    {
-        enemyPlaces.Add(FieldTileUtility.GetKeyFromCoord(5,5));
-    }
+  public EnemyPlaceHolder()
+  {
+    enemyPlaces.Add(FieldTileUtility.GetKeyFromCoord(5,5));
+    enemyPlaces.Add(FieldTileUtility.GetKeyFromCoord(9, 5));
+    enemyPlaces.Add(FieldTileUtility.GetKeyFromCoord(10, 11));
+  }
 
-		public int getEnemyStartTileKey()
-		{
-			return enemyPlaces[0];
-		}
+  public int getEnemyStartTileKey()
+  {
+    return enemyPlaces[0];
+  }
 }
