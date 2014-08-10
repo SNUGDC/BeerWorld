abc2a4e0a386820ea4db05c505f0a66f5dd38ead
MSapiens <kiki9014@gmail.com>
2014-08-10 16:37:07 +0900

    add login and signin

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -5,6 +5,10 @@ public class Login : MonoBehaviour {
 	string username="";
 	float height_unit = Screen.height / 720f * 1.4f;
 	float width_unit =  Screen.width / 1280f * 1.4f;
+	string url = "http://localhost:8880/";
+	string response;
+	bool can_login = false;
+	bool sign_in = false;
 	public GUISkin guiskin;
 
 	// Use this for initialization
@@ -17,6 +21,15 @@ public class Login : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(can_login){
+			can_login = false;
+			StartCoroutine(Request2Web("login"));
+			//Application.LoadLevel("Lobby");
+		}
+		if(sign_in){
+			can_login = false;
+			StartCoroutine(Request2Web("signin"));
+		}
 	
 	}
 
@@ -25,8 +38,31 @@ public class Login : MonoBehaviour {
 		username = GUI.TextField(new Rect(10*width_unit,10*height_unit,150*width_unit,40*height_unit),username);
 		PlayerPrefs.SetString ("id", username);
 		if(GUI.Button(new Rect(10*width_unit,60*height_unit,150*width_unit,50*height_unit),"Login")){
+			can_login = true;
+			//StartCoroutine(Login2Web());
+
 			//username = "";
-			Application.LoadLevel("Lobby");
+		}
+		if(GUI.Button (new Rect(10*width_unit,115*height_unit,150*width_unit,50*height_unit),"Sign In")){
+			sign_in = true;
 		}
 	}
+
+	IEnumerator Request2Web(string req){
+		Debug.Log ("Start " + req + "... : " + username);
+		WWWForm webform = new WWWForm();
+		webform.AddField("id",username);
+		WWW webReq = new WWW(url + req, webform);
+		yield return webReq;
+
+		Debug.Log ("Finished " + req + "... : " + webReq);
+		response = webReq.text;
+		if(response == "OK"){
+			Application.LoadLevel("Lobby");
+		}
+		else{
+			Debug.Log(response);
+		}
+	}
+
 }
