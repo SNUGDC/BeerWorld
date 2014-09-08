ab64e52f32cf03f876816cdd0ddff2de8b2714d1
Minjung Kim <Reny.atkins@gmail.com>
2014-09-08 21:54:11 +0900

    Divide BattlePlayer constructor. Unit(Enemy) / Character

--- Assets/Scripts/Battle/BattleUtil.cs
+++ Assets/Scripts/Battle/BattleUtil.cs
@@ -21,7 +21,33 @@ public class BattleUtil
         int maxHp = unit.maxHp;
         int currentHp = unit.currentHp;
 
+        int bounsStat = 0;
+
         return new BattlePlayer(attackDices, defenseDices,
-                maxHp, currentHp, ui);
+                maxHp, currentHp, bounsStat, ui);
+    }
+
+    public static BattlePlayer GetPlayer(Character unit, BattlePlayerUI ui)
+    {
+        List<BDice.Species> attackDices = new List<BDice.Species>();
+        List<BDice.Species> defenseDices = new List<BDice.Species>();
+        
+        for(int i = 0; i < unit.numberOfAttackDice; i++)
+        {
+            attackDices.Add(unit.speciesOfAttackDice);
+        }
+        
+        for(int i = 0; i < unit.numberOfDefenseDice; i++)
+        {
+            defenseDices.Add(unit.speciesOfDefenseDice);
+        }
+        
+        int maxHp = unit.maxHp;
+        int currentHp = unit.currentHp;
+
+        int bonusStat = unit.GetBonusStat();
+        
+        return new BattlePlayer(attackDices, defenseDices,
+                                maxHp, currentHp, bonusStat, ui);
     }
 }
