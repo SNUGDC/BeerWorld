a786607f444c00db2d6d925cefc7f4b7090d5975
Juhyeong Park <majecty@gmail.com>
2014-09-10 16:06:41 +0900

    Remove not used networkmanager cheat.

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -32,34 +32,12 @@ public partial class NetworkManager : MonoBehaviour {
 	void Start () {
 		username = PlayerPrefs.GetString ("id");
 		Id = Network.AllocateViewID ();
-
-	}
-
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.A)){
-
-			//networkView.RPC("StartGame", RPCMode.AllBuffered, Id, transform.position, Quaternion.identity);
-			player = Network.Instantiate (target, transform.position, Quaternion.identity,0 ) as GameObject;
-
-		}
-		if(Input.GetKeyDown(KeyCode.D)){
-			Network.Destroy(player);
-		}
-		if(Input.GetKey(KeyCode.W)){
-			player.transform.position += new Vector3(0,1,0);
-		}
-		if(Input.GetKey(KeyCode.S)){
-			player.transform.position += new Vector3(0,-1,0);
-		}
-
 	}
 
 	[RPC]
 	private void StartGame(NetworkViewID id, Vector3 pos, Quaternion rot){
 		//implement if func when started
 	}
-
 
 	//public static void Send
 
