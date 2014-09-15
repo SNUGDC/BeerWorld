3651077d30abe70f8e7a888f725b37152ef0186e
Juhyeong Park <majecty@gmail.com>
2014-09-16 07:23:30 +0900

    BattleUIManger and leftUI script.

---
+++ Assets/Scripts/UI/InGame/BattleUIManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections;
+
+public class BattleUIManager : MonoBehaviour
+{
+	public LeftUI leftUI;
+}
---
+++ Assets/Scripts/UI/InGame/LeftUI.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class LeftUI
+{
+	public List<GameObject> playerUIs;
+}
