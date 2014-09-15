ef149089ebd2e5a9cc5b972a82fd093e5eb7ca35
Minjung Kim <Reny.atkins@gmail.com>
2014-09-15 23:52:35 +0900

    Making different type enemy method. fixing...

---
+++ Assets/Scripts/Unit/EnemyInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemyInfo : MonoBehaviour {
+
+    public int enemyPlaceTileKey;
+    public Enemy.EnemyType enemyType; 
+
+    public EnemyInfo (int tileKey, Enemy.EnemyType type)
+    {
+        enemyPlaceTileKey = tileKey;
+        enemyType = type;
+    }
+}
---
+++ Assets/Scripts/Unit/EnemyInfoHolder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyInfoHolder : MonoBehaviour {
+
+/*    private List<EnemyInfo> enemyInfoList = new List<EnemyInfo>();
+
+    public int enemyPlaceTileKey;
+    public Enemy.EnemyType enemyType; 
+    
+    public EnemyInfo (int tileKey, Enemy.EnemyType type)
+    {
+        enemyPlaceTileKey = tileKey;
+        enemyType = type;
+    }
+
+    public EnemyInfoHolder()
+    {
+        //Smallest Enemies.
+        enemyInfoList.Add(new EnemyInfo(FieldTileUtility.GetKeyFromCoord(6, 4), Enemy.EnemyType.Smallest));
+        enemyInfoList.Add(new EnemyInfo(FieldTileUtility.GetKeyFromCoord(10, 4), Enemy.EnemyType.Smallest));
+        enemyInfoList.Add(new EnemyInfo(FieldTileUtility.GetKeyFromCoord(11, 11), Enemy.EnemyType.Smallest));
+        
+        //Middle Enemies.
+        enemyInfoList.Add(new EnemyInfo(FieldTileUtility.GetKeyFromCoord(5, 5), Enemy.EnemyType.Middle));
+        enemyInfoList.Add(new EnemyInfo(FieldTileUtility.GetKeyFromCoord(9, 5), Enemy.EnemyType.Middle));
+        enemyInfoList.Add(new EnemyInfo(FieldTileUtility.GetKeyFromCoord(10, 11), Enemy.EnemyType.Middle));
+    }
+    
+    public EnemyInfo getFirstEnemyInfo()
+    {
+        return enemyInfoList[0];
+    }
+    
+    public List<EnemyInfo> GetEnemyInfoList()
+    {
+        return enemyInfoList;
+    }*/
+}
