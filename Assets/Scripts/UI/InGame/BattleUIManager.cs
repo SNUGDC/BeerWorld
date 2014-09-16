410f590ebe2eb6f31270de6850e59102b1409f6c
Juhyeong Park <majecty@gmail.com>
2014-09-17 06:52:57 +0900

    Enable ui when user enter.

--- Assets/Scripts/UI/InGame/BattleUIManager.cs
+++ Assets/Scripts/UI/InGame/BattleUIManager.cs
@@ -1,7 +1,37 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Smooth.Slinq;
 
 public class BattleUIManager : MonoBehaviour
 {
 	public LeftUI leftUI;
+	public List<LeftUIComps.Player> players;
+
+	private static BattleUIManager instance = null;
+	public static BattleUIManager Get()
+	{
+		return instance;
+	}
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void SetPlayers(List<NetworkViewID> playerIds)
+	{
+		if (playerIds.Count > leftUI.playerUIs.Count)
+		{
+			Debug.LogError("Not enough UI slot for character");
+		}
+
+		Queue<GameObject> playerUIs = new Queue<GameObject>(leftUI.playerUIs);
+		var players = Slinqable.Slinq(playerIds).Select((playerId) => {
+			var playerUI = playerUIs.Dequeue();
+			return new LeftUIComps.Player(playerUI, playerId);
+		});
+
+		this.players = players.ToList();
+	}
 }
--- Assets/Scripts/UI/InGame/LeftUI.cs
+++ Assets/Scripts/UI/InGame/LeftUI.cs
@@ -7,3 +7,28 @@ public class LeftUI
 {
 	public List<GameObject> playerUIs;
 }
+
+namespace LeftUIComps
+{
+	[System.Serializable]
+	public class Player
+	{
+		[SerializeField]
+		private GameObject playerUI;
+		[SerializeField]
+		private NetworkViewID playerId;
+
+		public Player(GameObject playerUI, NetworkViewID playerId)
+		{
+			this.playerUI = playerUI;
+			this.playerId = playerId;
+
+			EnableUI();
+		}
+
+		void EnableUI()
+		{
+			playerUI.SetActive(true);
+		}
+	}
+}
