5b4093ae6b11f0f66081bfba7f51d8acc6b783db
yulcat <alszmtm@naver.com>
2014-09-20 12:35:35 +0900

    Pop Message on Escape

---
+++ Assets/EscMsg.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscMsg : MonoBehaviour {
+	private Animator anim;
+	private TextMesh txt;
+	public string[] messages;
+	void Awake(){
+		anim = GetComponent<Animator>();
+		txt = GetComponent<TextMesh>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			anim.SetTrigger("PopMsg");
+			txt.text = messages[(int)(messages.Length * Random.value)];
+		}
+	}
+}
