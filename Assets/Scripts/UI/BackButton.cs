fe8bb898624baceeab8efdc4074078b49e1335a4
MSapiens <kiki9014@gmail.com>
2014-09-20 14:18:34 +0900

    add UI in Ready Scene

--- Assets/Scripts/UI/BackButton.cs
+++ Assets/Scripts/UI/BackButton.cs
@@ -2,14 +2,19 @@
 using System.Collections;
 
 public class BackButton : MonoBehaviour {
+	RoomManager rmManager;
 
 	// Use this for initialization
 	void Start () {
-	
+		rmManager = FindObjectOfType<RoomManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnMouseUp(){
+		rmManager.DisconnectFromServer ();
 	}
 }
---
+++ Assets/Scripts/UI/MyCharacter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCharacter : MonoBehaviour {
+	string m_currCharacter = "Warrior";
+	SpriteRenderer spRender;
+	bool ready;
+	int myID;
+
+	// Use this for initialization
+	void Start () {
+		spRender = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	string currCharacter{ 
+		get{ 
+			return m_currCharacter;
+		} 
+	}
+
+	public void changeCharacter(int move){
+		int currState = (int) Character.CharClass.Parse (typeof(Character.CharClass),m_currCharacter) + move;
+		if(currState < 1){
+			currState = 3;
+		}
+		else if(currState > 3){
+			currState = 1;
+		}
+		m_currCharacter = ((Character.CharClass) currState).ToString();
+		Sprite sprite = Resources.Load("Images/mainUI/components/lobby_char_"+m_currCharacter,typeof(Sprite)) as Sprite;
+		spRender.sprite = sprite;
+	}
+
+	public void Ready(bool _ready){
+		ready = _ready;
+	}
+}
