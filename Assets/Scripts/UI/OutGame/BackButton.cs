508830d681cf89e6df901c1db8f5f40925f4a1d1
MSapiens <kiki9014@gmail.com>
2014-09-21 00:56:35 +0900

    Add Sound to Button in OutGame

---
+++ Assets/Scripts/UI/OutGame/BackButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackButton : MonoBehaviour {
+	RoomManager rmManager;
+
+	// Use this for initialization
+	void Start () {
+		rmManager = FindObjectOfType<RoomManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+		rmManager.DisconnectFromServer ();
+		audio.Play ();
+	}
+}
---
+++ Assets/Scripts/UI/OutGame/StageMatchingButton.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class StageMatchingButton : MonoBehaviour {
+	RoomManager rmManager;
+	public string mapName = "France"; 
+	public GameObject loadingImage;
+	GameObject loading;
+
+	// Use this for initialization
+	void Start () {
+		rmManager = FindObjectOfType<RoomManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+		rmManager.UpdateMasterServerInfo();
+		SearchRoom ("France");
+		audio.Play ();
+		//loading = Instantiate (loadingImage) as GameObject;
+	}
+
+	void OnQueue(){
+		HostData[] roomList = MasterServer.PollHostList();
+		if(roomList.Length == 0){
+			//Destroy(loading);
+			CreateRoom(SystemInfo.deviceUniqueIdentifier, mapName);
+			Debug.Log("No Room in Server. So Create the Room.");
+			return;
+		}
+		else{
+			foreach (HostData room in roomList){
+				if(room.connectedPlayers<3){
+					//Destroy(loading);
+					JoinRoom(room);
+					Debug.Log("Find Room. So Enter the Room.");
+					return;
+				}
+			}
+			//Destroy(loading);
+			CreateRoom(SystemInfo.deviceUniqueIdentifier, mapName);
+			Debug.Log("There Are No Room Available. So Create the Room.");
+			return;
+		}
+	}
+}
+
+public partial class StageMatchingButton : MonoBehaviour
+{
+	void CreateRoom(string roomName, string roomType)
+	{
+		Network.InitializeServer(3, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(roomType, roomName, "testing");
+	}
+	
+	void JoinRoom(HostData element)
+	{
+		NetworkConnectionError error = Network.Connect(element);
+		Debug.Log("Join Room : " + error);
+	}
+	
+	void SearchRoom(string roomType)
+	{
+		Debug.Log ("Search Room. " + "MasterServerIP : " + MasterServer.ipAddress + "Port : " + MasterServer.port);
+		MasterServer.ClearHostList();
+		MasterServer.RequestHostList(roomType);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived) {
+			Debug.Log("List Received");
+			OnQueue();
+		}
+	}
+}
