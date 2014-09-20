8953babc05917ac4fcdf16a0656b025a35d9a947
Juhyeong Park <majecty@gmail.com>
2014-09-21 00:26:56 +0900

    Remove unused logs.

--- Assets/Scripts/UI/InGame/BattleUIManager.cs
+++ Assets/Scripts/UI/InGame/BattleUIManager.cs
@@ -136,12 +136,6 @@ public class BattleUIManager : MonoBehaviour
 		return Run.After(0.3f, () => { })
 		.Then(
 			Run.WaitWhile(() => effectSpriteRenderer.enabled, false)
-		)
-		.ExecuteWhenDone(
-			() => Debug.Log("Use item effect done.")
-		)
-		.ExecuteWhenDone(
-			() => Debug.Log("JEWIOJIOEJFW")
 		);
 	}
 
@@ -165,7 +159,6 @@ public class BattleUIManager : MonoBehaviour
 		Slinqable.Slinq(inventoryUI.itemCardComps)
 			.ForEach((itemCard) => itemCard.SetItem(Character.Item.None, null));
 
-		Debug.Log("First of items is " + items[0] + ", " + items.Count);
 		items.ForEach(
 			(item) => { AddItemCard(item); }
 		);
@@ -183,7 +176,6 @@ public class BattleUIManager : MonoBehaviour
 
 		ShowItemUseAnimation(item)
 			.ExecuteWhenDone(() => {
-				Debug.Log("Not delete item");
 				var characterManager = GameManager.GetMyCharacterManager();
 				var character = characterManager.GetCharacterInstance();
 				character.UseItem(item);
