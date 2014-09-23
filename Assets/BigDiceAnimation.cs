c3c4507d99b22155f908b30ccada6392c2f4503e
yulcat <alszmtm@naver.com>
2014-09-23 13:43:58 +0900

    dice gauge color fix, dice animation minimum time 0.5f, dice after-roll animation shortened

--- Assets/BigDiceAnimation.cs
+++ Assets/BigDiceAnimation.cs
@@ -6,6 +6,8 @@ public class BigDiceAnimation : MonoBehaviour {
 	int resultNum;
 	public delegate void DiceGetter(int diceResult);
 	public DiceGetter diceGetter;
+	bool TimeLimitPassed;
+	bool mouseIsUp;
 
 	// Use this for initialization
 	void Start () {
@@ -25,15 +27,29 @@ public class BigDiceAnimation : MonoBehaviour {
 		multiAudioClip.audioSources[0].playOnAwake=false;
 		multiAudioClip.audioSources[0].loop = true;
 		multiAudioClip.audioSources[0].Play ();
+		TimeLimitPassed = false;
+		mouseIsUp = false;
+		Invoke ("enableRoll",0.5f);
 	}
+
 	void OnMouseUp(){
-		StartCoroutine("ShowRandomDice");
+		if(TimeLimitPassed)
+			StartCoroutine("ShowRandomDice");
+		else
+			mouseIsUp = true;
 	}
+
+	void enableRoll(){
+		TimeLimitPassed = true;
+		if(mouseIsUp)
+			StartCoroutine("ShowRandomDice");
+	}
+
 	IEnumerator ShowRandomDice(){
 		anim.SetTrigger("stopRoll");
 		MultiAudioClip multiAudioClip = GetComponent<MultiAudioClip>();
 		for(int j=1; j<4; j++){
-			for(int i=0; i<4; i++){
+			for(int i=0; i<2; i++){
 				multiAudioClip.audioSources[0].loop=false;
 				multiAudioClip.audioSources[0].Play ();
 				int num = (int)(Random.value*6)+1;
