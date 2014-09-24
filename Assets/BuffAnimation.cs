1e27987c9a94e25da6db861084997b1f6bfdb182
yulcat <alszmtm@naver.com>
2014-09-24 21:37:15 +0900

    changed bufficon movement to local

--- Assets/BuffAnimation.cs
+++ Assets/BuffAnimation.cs
@@ -14,7 +14,9 @@ public class BuffAnimation : MonoBehaviour {
 	public void PlayBuffAt(Vector3 tilePos, int iconNum){
 		Vector3 translatedPos;
 		translatedPos = uiCam.ScreenToWorldPoint(mainCam.WorldToScreenPoint(tilePos));
-		iTween.MoveFrom(BuffIcons[iconNum],iTween.Hash("position",translatedPos,"easetype",iTween.EaseType.easeInCubic,"time",0.5f));
-		iTween.ScaleFrom(BuffIcons[iconNum],iTween.Hash("scale",Vector3.one*3f,"easetype",iTween.EaseType.easeInCubic,"time",0.5f));
+		BuffIcons[iconNum].transform.InverseTransformPoint(translatedPos);
+		translatedPos = BuffIcons[iconNum].transform.parent.InverseTransformPoint(translatedPos);
+		iTween.MoveFrom(BuffIcons[iconNum],iTween.Hash("position",translatedPos,"easetype",iTween.EaseType.easeInCubic,"time",0.5f,"islocal",true));
+		iTween.ScaleFrom(BuffIcons[iconNum],iTween.Hash("scale",Vector3.one*3f,"easetype",iTween.EaseType.easeInCubic,"time",0.5f,"islocal",true));
 	}
 }
